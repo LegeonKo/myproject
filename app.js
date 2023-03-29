fab93653a641eb33704a4ed7b58803796732e1e2
LegeoN <legeon0990@Yandex.ru>
2023-03-29 23:09:10 +0300

    new blocks

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 import '/assets/app.sass'
 import '/assets/fonts/stylesheet.css'
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        startPosition: 2,
+        autoWidth:true,
+        center: true,
+        items:4,
+        loop:true,
+        margin:25
+    });
+});
